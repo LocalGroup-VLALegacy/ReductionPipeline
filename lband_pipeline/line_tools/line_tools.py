f97bdaacebc8fc930e04879b773a064ce435a903
e-koch <koch.eric.w@gmail.com>
2020-05-20 13:33:01 -0700

    Fixes for bandpass gap interpolation

--- lband_pipeline/line_tools/line_tools.py
+++ lband_pipeline/line_tools/line_tools.py
@@ -23,9 +23,21 @@ def bandpass_with_gap_interpolation(myvis, context, refantignore="",
     '''
 
     # Look for BP table
-    bpname = glob("{0}.{1}.s*_4.{1}BPcal.tbl".format(myvis, task_string, search_string))
-    assert len(bpname) == 1
-    bpname = bpname[0]
+    bpname = glob("{0}.{1}.s*_4.{2}BPcal.tbl".format(myvis, task_string, search_string))
+
+    # test and final cal steps will have 1 match:
+    if len(bpname) == 1:
+        bpname = bpname[0]
+    elif len(bpname) == 2:
+        # One table for each semifinal cal call.
+        # Grab the second call table.
+        bpname = sorted(bpname,
+                        key=lambda x: int(x.split("_4.BPcal.tbl")[0].split(".s")[-1]))[-1]
+    elif len(bpname) == 0:
+        raise ValueError("No matches to the BP table found.")
+    else:
+        raise ValueError("Found too many matches to the BP table."
+                         " Unclear which should be used: {0}".format(bpname))
 
     # Remove already-made version
     # rmtables(bpname)
@@ -64,15 +76,47 @@ def bandpass_with_gap_interpolation(myvis, context, refantignore="",
         priorcals.extend(ant_tbl)
 
     del_tbl = glob("{0}.{1}.s*_2.{2}delay.tbl".format(myvis, task_string, search_string))
-    assert len(del_tbl) == 1
 
-    BPinit_tbl = glob("{0}.{1}.s*_3.{2}BPdinitialgain.tbl".format(myvis,
-                                                                  task_string,
-                                                                  search_string))
-    assert len(BPinit_tbl) == 1
+    # test and final cal steps will have 1 match:
+    if len(del_tbl) == 1:
+        del_tbl = del_tbl[0]
+    elif len(del_tbl) == 2:
+        # One table for each semifinal cal call.
+        # Grab the second call table.
+        del_tbl = sorted(del_tbl,
+                        key=lambda x: int(x.split("_2.delay.tbl")[0].split(".s")[-1]))[-1]
+    elif len(del_tbl) == 0:
+        raise ValueError("No matches to the BP table found.")
+    else:
+        raise ValueError("Found too many matches to the delay table."
+                         " Unclear which should be used: {0}".format(del_tbl))
+
+    # Slight difference in BP initial gain table names
+    if task_string == "hifv_testBPdcals":
+        tabname_string = "BPdinitialgain"
+    else:
+        tabname_string = "BPinitialgain"
+
+    BPinit_tbl = glob("{0}.{1}.s*_3.{2}{3}.tbl".format(myvis,
+                                                       task_string,
+                                                       search_string,
+                                                       tabname_string))
+    # test and final cal steps will have 1 match:
+    if len(BPinit_tbl) == 1:
+        BPinit_tbl = BPinit_tbl[0]
+    elif len(BPinit_tbl) == 2:
+        # One table for each semifinal cal call.
+        # Grab the second call table.
+        BPinit_tbl = sorted(BPinit_tbl,
+                        key=lambda x: int(x.split("_3.{}.tbl".format(tabname_string))[0].split(".s")[-1]))[-1]
+    elif len(BPinit_tbl) == 0:
+        raise ValueError("No matches to the BP table found.")
+    else:
+        raise ValueError("Found too many matches to the BP initgain table."
+                         " Unclear which should be used: {0}".format(BPinit_tbl))
 
     gaintables = copy(priorcals)
-    gaintables.extend([del_tbl[0], BPinit_tbl[0]])
+    gaintables.extend([del_tbl, BPinit_tbl])
 
     bandpass(vis=myvis,
              caltable=bpname,
