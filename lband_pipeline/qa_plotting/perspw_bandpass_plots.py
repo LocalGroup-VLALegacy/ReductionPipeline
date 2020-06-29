168c088d5792b31144661de0436747761c30cae9
e-koch <koch.eric.w@gmail.com>
2020-06-29 16:21:51 -0600

    Add exporting txt files for BP table

--- lband_pipeline/qa_plotting/perspw_bandpass_plots.py
+++ lband_pipeline/qa_plotting/perspw_bandpass_plots.py
@@ -83,3 +83,67 @@ def make_spw_bandpass_plots(ms_active,
                 fontsize=10.0,
                 showgui=False,
                 figfile=os.path.join(bp_folder, filename))
+
+
+def make_bandpass_txt(ms_active, output_folder='finalBPcal_txt'):
+    '''
+    Output an amplitude and phase txt file from the bandpass
+    table.
+
+    Expects to find a the "*.finalBPcal.tbl" produced by the VLA pipeline.
+    A `ValueError` is raise when no final BP cal table is found.
+
+    Two output text files are created: freq. vs. amp and freq. vs. phase.
+
+    '''
+
+    from taskinit import tb
+
+    from tasks import plotms
+
+    mySDM = ms_active.rstrip(".ms")
+
+    tb.open(ms_active + "/SPECTRAL_WINDOW")
+    nspws = tb.getcol("NAME").shape[0]
+    tb.close()
+
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+
+    # Final BP cal table now includes the stage number and step
+    finalbpcal_name = glob(mySDM + '*.finalBPcal.tbl')
+    if len(finalbpcal_name) == 0:
+        raise ValueError("Cannot find finalBPcal table name.")
+    # Blindly assume we want the first name
+    finalbpcal_name = finalbpcal_name[0]
+
+    # Make txt files per SPW.
+    for ii in range(nspws):
+
+        # Output text names
+        amp_filename = '{0}_amp_spw{1}.txt'.format(os.path.splitext(finalbpcal_name)[0], ii)
+        phase_filename = '{0}_phase_spw{1}.txt'.format(os.path.splitext(finalbpcal_name)[0], ii)
+
+        plotms(vis=finalbpcal_name,
+               xaxis='freq',
+               yaxis='amp',
+               field='',
+               antenna='',
+               spw=str(ii),
+               timerange='',
+               showgui=False,
+               # avgtime='1e8',
+               averagedata=True,
+               plotfile=os.path.join(output_folder, amp_filename))
+
+        plotms(vis=finalbpcal_name,
+               xaxis='freq',
+               yaxis='phase',
+               field='',
+               antenna='',
+               spw=str(ii),
+               timerange='',
+               showgui=False,
+               # avgtime='1e8',
+               averagedata=True,
+               plotfile=os.path.join(output_folder, phase_filename))
