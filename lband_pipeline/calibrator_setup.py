7b031d9ab229a21bc3f493bd0748f86e28e3a4bb
e-koch <koch.eric.w@gmail.com>
2021-03-15 17:38:19 -0400

    Add new calibrator sources (still need HI velocity ranges for some)

--- lband_pipeline/calibrator_setup.py
+++ lband_pipeline/calibrator_setup.py
@@ -14,14 +14,29 @@ calibrator_line_range_kms = {}
 # Velocities are in km/s in the LSRK frame.
 
 # --------------------------------------------------------------
-# 3C48
+# Flux/BP/delay calibrators
+# --------------------------------------------------------------
+
+# 3C48 (WLM; IC10; IC1613; M31; M33)
 # Fairly weak absorption, mostly from 0 to -40 km /s
 calibrator_line_range_kms['3C48'] = {}
 calibrator_line_range_kms['3C48']['HI'] = [50, -50]
 
+# 1331+305=3C286 (NGC6822)
+# Also a polarization angle cal
+# 3C286
+# Fairly weak and narrow absorption lines.
+calibrator_line_range_kms['3C286'] = {}
+calibrator_line_range_kms['3C286']['HI'] = [30, -60]
+calibrator_line_range_kms['1331+305=3C286'] = {}
+calibrator_line_range_kms['1331+305=3C286']['HI'] = [30, -60]
 
 # --------------------------------------------------------------
-# 3C138
+# Polarization angle calibrators
+# --------------------------------------------------------------
+# NOTE: 3C286 is listed above already
+
+# 3C138 (IC10; IC1613; M31; WLM; M33)
 # Pol angle / backup flux/BP cal
 # Strong absorption from 60 to -40 km/s
 calibrator_line_range_kms['3C138'] = {}
@@ -29,23 +44,45 @@ calibrator_line_range_kms['3C138']['HI'] = [70, -70]
 
 
 # --------------------------------------------------------------
-# J0029+3456
-# M31 phase cal. (at least C/D-config)
-# Strong absorption from ~12 to -30 km/s
-calibrator_line_range_kms['J0029+3456'] = {}
-calibrator_line_range_kms['J0029+3456']['HI'] = [30, -50]
-
-
+# Polarization leakage calibrators
 # --------------------------------------------------------------
-# J0038+4137
-# M31 phase cal. (at least C/D-config)
-# No strong absorption. Pick fairly narrow region to be safe.
-calibrator_line_range_kms['J0038+4137'] = {}
-calibrator_line_range_kms['J0038+4137']['HI'] = [30, -30]
-
-# --------------------------------------------------------------
-# J0319+4130
+# J0319+4130 (M31; IC10; WLM; M33)
 # Pol leakage (at least C/D-config)
 # Strong absorption from 30 to -40 km/s
 calibrator_line_range_kms['J0319+4130'] = {}
 calibrator_line_range_kms['J0319+4130']['HI'] = [50, -60]
+
+# J2355+4950 (IC1613)
+# Pol leakage
+
+# J1407+2827 (NGC6822)
+# Pol leakage
+
+# --------------------------------------------------------------
+# Gain calibrators
+# --------------------------------------------------------------
+# NOTE: 3C48 is the gain cal for M33
+
+# J0029+3456 (M31)
+# Gain cal. (at least C/D-config)
+# Strong absorption from ~12 to -30 km/s
+calibrator_line_range_kms['J0029+3456'] = {}
+calibrator_line_range_kms['J0029+3456']['HI'] = [30, -50]
+
+# J0038+4137 (M31)
+# Gain cal
+# No strong absorption. Pick fairly narrow region to be safe.
+calibrator_line_range_kms['J0038+4137'] = {}
+calibrator_line_range_kms['J0038+4137']['HI'] = [30, -30]
+
+# J0059+0006 (IC1613)
+# Gain cal
+
+# J1923-2104 (NGC6822)
+# Gain cal
+
+# J0102+5824 (IC10)
+# Gain cal
+
+# J2321-1623 (WLM)
+# Gain cal
