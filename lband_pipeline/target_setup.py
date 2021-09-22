a340e19b60ed6d1e356a86e1f2e17ae4bdb8bf9d
e-koch <koch.eric.w@gmail.com>
2021-09-22 11:16:26 -0400

    Add separate function to identify which of our galaxies is observed in a track

--- lband_pipeline/target_setup.py
+++ lband_pipeline/target_setup.py
@@ -11,6 +11,11 @@ smaller range to be flagged instead of the whole galaxy range (e.g., M31).
 
 '''
 
+from casatools import logsink
+
+casalog = logsink()
+
+
 # km/s in LSRK
 target_vsys_kms = {"M31": -296.,
                    "M33": -180.,
@@ -118,3 +123,52 @@ target_line_range_kms['NGC6822'] = {}
 target_line_range_kms['NGC6822']['HI'] = [[80, -150]]
 
 target_line_range_kms['NGC6822']['OH'] = [[-10, -120]]
+
+
+# Function to identify the target from field names in the MS
+
+def identify_target(vis, fields=None, raise_missing_target=True):
+    '''
+    Identify the target in the MS that matches target_line_range_kms keys.
+    '''
+
+    if fields is None:
+        fields = []
+
+    from casatools import ms
+
+    myms = ms()
+
+    # if no fields are provided use observe_target intent
+    # I saw once a calibrator also has this intent so check carefully
+    # mymsmd.open(vis)
+    myms.open(vis)
+
+    mymsmd = myms.metadata()
+
+    if len(fields) < 1:
+        fields = mymsmd.fieldsforintent("*TARGET*", True)
+
+    if len(fields) < 1:
+        casalog.post("ERROR: no fields given to identify.")
+        return
+
+    # generate a dictonary containing continuum chunks for every spw of every field
+    for field in fields:
+        spws = mymsmd.spwsforfield(field)
+
+        # Match target with the galaxy. Names should be unique enough to do this
+        thisgal = None
+        for gal in target_line_range_kms:
+            if gal in field:
+                thisgal = gal
+                break
+        # Check for match
+        if thisgal is None:
+            if raise_missing_target:
+                raise ValueError("Unable to match field {} to expected galaxy targets".format(field))
+            else:
+                casalog.post("Unable to match field {} to expected galaxy targets. Skipping.".format(field))
+                continue
+
+        return thisgal
